680520a6d4ab12d6ef82482d8ddb12f86b9e28ee
gabrielyvg <gabrielyvg@outlook.com>
2024-01-22 22:58:28 -0300

    initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+            <section className="mt-6">
+                <div>
+                    <h1>Olá</h1>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ src/utils/mock-obj.tsx
@@ -0,0 +1,35 @@
+const navbar = [
+    {
+        id: 1,
+        title: "Straw hats",
+    },
+    {
+        id: 2,
+        title: "White Beard Pirates",
+    },
+    {
+        id: 3,
+        title: "Seven Warlords of the Sea",
+    },
+];
+
+const images = [
+    {
+        id: 1,
+        alt: '',
+        url: '',
+    },
+    {
+        id: 2,
+        alt: '',
+        url: '',
+    },
+    {
+        id: 3,
+        alt: '',
+        url: '',
+    },
+];
+
+
+export default { navbar, images };
