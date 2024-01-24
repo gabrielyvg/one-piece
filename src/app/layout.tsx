0d583413a920bcc709853c99dff9bf18c01238f3
gabrielyvg <gabrielyvg@outlook.com>
2024-01-23 22:19:24 -0300

    Adicionando a header. Colocado o favicon. Ajustada a cor de fundo. Começando a preparar a estrutura de pastas para os modelos 3d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import '@/styles/globals.css'
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Favicon from '/public/images/flag.png';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "One Piece",
   description: ":D",
+  icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout({
@@ -18,11 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header/>
-          {children}
-        <Footer/>
+    <html lang="en" className="bg-cyan-200">
+      <body>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,32 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <>
+            <div className="w-full h-10 sticky top-0">
+                <div className="container mx-auto px-4 h-full">
+                    <div className="flex justify-between items-center h-full">
+                        {/* <Logo */}
+                        <ul className="hidden md:flex gap-x-6 text-black">
+                            <li>
+                                <Link href="#">
+                                    <p>Straw hats</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#">
+                                    <p>White Beard Pirates</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#">
+                                    <p>Seven Warlords of the Sea</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
